2c4893df4356b1578703d4c3fc78b9b395aabcd5
franketa <francohbalik@gmail.com>
2021-08-28 16:39:10 -0300

    just needs instructions

--- main.py
+++ main.py
@@ -3,18 +3,78 @@ import datetime as dt
 import pandas_datareader.data as web
 import plotly.express as px
 import plotly.graph_objects as go
-start = dt.datetime(2020,8,8)
+start = dt.datetime(2020, 8, 8)
 end = dt.datetime.now()
+selected_stock = 'AMZN'
+stocks = web.DataReader(selected_stock, 'yahoo', start, end)
+stocks_close = pd.DataFrame(web.DataReader(
+    selected_stock, 'yahoo', start, end)['Close'])
+stocks2 = pd.DataFrame(web.DataReader(selected_stock, 'yahoo', start, end))
+print(stocks2['Close'])
+#stocks2['Close',selected_stock] = stocks2.rolling(5).mean()
+df = web.DataReader(selected_stock, 'yahoo', start, end)
+lows=df.Low.rolling(window=2, min_periods=1).mean()
+highs = df.High.rolling(window=2, min_periods=1).mean()
+mids = df.Close.rolling(window=2, min_periods=1).mean()
+trace1 = {
+    'x': df.index,
+    'y': mids,
+    'type': 'scatter',
+    'mode': 'lines',
+    'line': {
+        'width': 1,
+        'color': 'black'
+    },
+    'name': 'Moving Average of 30 periods'
+}
+trace2 = {
+    'x': df.index,
+    'y': highs,
+    'type': 'scatter',
+    'mode': 'lines',
+    'line': {
+        'width': 1,
+        'color': 'blue'
+    },
+    'name': 'Moving Average of 30 periods'
+}
+trace3 = {
+    'x': df.index,
+    'y': lows,
+    'type': 'scatter',
+    'mode': 'lines',
+    'line': {
+        'width': 1,
+        'color': 'red'
+    },
+    'name': 'Moving Average of 30 periods'
+}
 
-stocks = web.DataReader(['FB','AMZN', 'AAPL', 'NFLX', 'GOOGL', 'MSFT'], 'yahoo', start, end)
-stocks_close = pd.DataFrame(web.DataReader(['FB','AMZN', 'AAPL', 'NFLX', 'GOOGL', 'MSFT'], 'yahoo', start, end)['Close'])
+c_candlestick = go.Figure(data=[go.Candlestick(x=stocks.index,
+                                               open=stocks[('Open')],
+                                               high=stocks[('High')],
+                                               low=stocks[('Low')],
+                                               close=stocks[('Close')]),
+                               trace1, trace2,trace3])
 
-# Area chart
+c_candlestick.update_xaxes(
+    title_text='Date',
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label='1M', step='month', stepmode='backward'),
+            dict(count=6, label='6M', step='month', stepmode='backward'),
+            dict(count=1, label='YTD', step='year', stepmode='todate'),
+            dict(count=1, label='1Y', step='year', stepmode='backward'),
+            dict(step='all')])))
 
-area_chart = px.area(stocks_close.NFLX, title = 'FACEBOOK SHARE PRICE (2013-2020)')
+c_candlestick.update_layout(
+    title={
+        'text': 'AMAZON SHARE PRICE (2013-2020)',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top'})
 
-area_chart.update_xaxes(title_text = 'Date')
-area_chart.update_yaxes(title_text = 'FB Close Price', tickprefix = '$')
-area_chart.update_layout(showlegend = False)
-
-area_chart.show()
+c_candlestick.update_yaxes(title_text='AMZN Close Price', tickprefix='$')
+c_candlestick.show()
